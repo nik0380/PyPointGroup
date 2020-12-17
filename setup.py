657272c00fc5ee5b3ef096b661da495aca97a191
nik0380 <somov@phys.unn.ru>
2020-12-17 18:29:42 +0300

    test-02

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 from setuptools import setup, find_packages
 
+#-----------------------------------------------------------------------------------------
+
 def load_description():
     with open('README.md','r') as f:
         text = f.read()
     return text
 
+#-----------------------------------------------------------------------------------------
+#
+#-----------------------------------------------------------------------------------------
+
 setup(
     name='PyPointGroup',
-    version='2.0.4',
+    version='2.0.5',
     packages=['pypointgroup', 'pypointgroup.gl', 'pypointgroup.gui', 'pypointgroup.gui.ui', 'pypointgroup.core'],
     url='https://github.com/nik0380/pypointgroup',
     license='MIT',
@@ -20,5 +26,4 @@ setup(
     install_requires=["PyQt5", "numpy","PyOpenGL"],
     include_package_data=True,
     entry_points={'console_scripts': ['pypointgroup=pypointgroup:main',],},
-
 )
